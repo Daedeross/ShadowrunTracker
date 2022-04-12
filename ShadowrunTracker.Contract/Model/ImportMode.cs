89dd3f34bc6884424612ca89136bd9fd3d55d931
Daedeross <daedeross@gmail.com>
2022-04-12 17:49:28 -0500

    Missed file

---
+++ ShadowrunTracker.Contract/Model/ImportMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShadowrunTracker.Model
+{
+    public enum ImportMode
+    {
+        New = 0,
+        Library,
+        File,
+        ChummerFile
+    }
+}
